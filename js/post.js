23b5cf7c7d21a317cda59219209b1a0bca45cb3e
joshuastray <joshua.astray.14@gmail.com>
2014-05-08 00:24:36 +0800

    1

--- js/post.js
+++ js/post.js
@@ -1,4 +1,6 @@
 $(document).ready(function(){
+	window.disqus_shortname = 'joshuasblog'; // required: replace example with your forum shortname
+	$.getScript('http://' + disqus_shortname + '.disqus.com/embed.js',function(){});
 
     var isMobile = {
         Android: function() {
@@ -22,13 +24,6 @@ $(document).ready(function(){
     };
 
     $('pre').addClass('prettyprint linenums'); //添加Google code Hight需要的class
-
-    window.disqus_shortname = 'joshuasblog'; // required: replace example with your forum shortname
-    $('#disqus_container .comment').on('click',function(){
-        $(this).html('加载中...');
-        var that = this;
-        $.getScript('http://' + disqus_shortname + '.disqus.com/embed.js',function(){$(that).remove()});
-    });
 
     $('.entry a').each(function(index,element){
         var href = $(this).attr('href');
